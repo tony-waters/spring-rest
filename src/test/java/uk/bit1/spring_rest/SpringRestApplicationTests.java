f121585611dd33e5ec384a6f88dac58b01563cf3
tony-waters <2tonywaters@gmail.com>
2026-01-05 14:01:41 +0000

    add spring initializr

---
+++ src/test/java/uk/bit1/spring_rest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.bit1.spring_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
